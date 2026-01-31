043323eedb0204efee37fb41cb0e51145f8dc9ea
hariom8194 <heyitshb1@gmail.com>
2026-01-31 22:53:18 +0530

    Assignment 3

---
+++ Assignment3/Task2.py
@@ -0,0 +1,8 @@
+import math
+num = int(input("Enter a number: "))
+sqroot = math.sqrt(num)
+logarithm = math.log(num)
+sine = math.sin(num)
+print(f"Square root: {sqroot}")
+print(f"Logarithm: {logarithm}")
+print(f"Sine: {sine}")
